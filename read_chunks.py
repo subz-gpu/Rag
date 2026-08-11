db5eb6a979d80a55dc73d9faf563aa18350f170a
Subham Dewan <subhamdewan07@gmail.com>
2026-08-11 22:28:05 +0530

    Save embeddings DataFrame to joblib file
    Added functionality to save the DataFrame of embeddings to a joblib file.

--- read_chunks.py
+++ read_chunks.py
@@ -4,6 +4,7 @@ import json
 import numpy as np
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
+import joblib
 
 def create_embedding(text_list):
     # https://github.com/ollama/ollama/blob/main/docs/api.md#generate-embeddings
@@ -31,20 +32,8 @@ for json_file in jsons:
         chunk['embedding'] = embeddings[i]
         chunk_id += 1
         my_dicts.append(chunk) 
-    break
-# print(my_dicts)
 
-df = pd.DataFrame.from_records(my_dicts) 
-incoming_query = input("Ask a Question: ")
-question_embedding = create_embedding([incoming_query])[0] 
+df = pd.DataFrame.from_records(my_dicts)
+# Save this dataframe
+joblib.dump(df, 'embeddings.joblib')
 
-# Find similarities of question_embedding with other embeddings
-# print(np.vstack(df['embedding'].values))
-# print(np.vstack(df['embedding']).shape)
-similarities = cosine_similarity(np.vstack(df['embedding']), [question_embedding]).flatten()
-print(similarities)
-top_results = 3
-max_indx = similarities.argsort()[::-1][0:top_results]
-print(max_indx)
-new_df = df.loc[max_indx] 
-print(new_df[["title", "number", "text"]])
